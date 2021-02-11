19761cdf2bcfeb064f256274adb430aa2726387e
atrox3d <atrox3d@gmail.com>
2021-02-11 17:40:33 +0100

    create snake, my solution

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from turtle import Turtle, Screen
-
+#
+#   setup screen
+#
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+#
+#   create snake body
+#
+snake = []
+offset = 0
+for _ in range(3):
+    s = Turtle("square")
+    s.color("white")
+    s.penup()
+    s.setx(offset)
+    snake.append(s)
+    offset -= 20
 
 
 screen.exitonclick()
