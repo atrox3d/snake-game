d9ab2abaa4711016b65bdec00df17e2a85715f36
atrox3d <atrox3d@gmail.com>
2021-02-12 07:23:27 +0100

    create movement

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from turtle import Turtle, Screen
 #
 #   setup screen
@@ -6,6 +7,7 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 #
 #   create snake body
 #
@@ -15,9 +17,30 @@ positions = [
     (-40, 0)
 ]
 
+snake = []
+
 for position in positions:
     segment = Turtle("square")
     segment.color("white")
+    segment.penup()
     segment.goto(position)
+    snake.append(segment)
+
+
+gameon = True
+head = snake[0]
+while gameon:
+    screen.update()
+    time.sleep(0.1)
+
+    for segment in range(len(snake)-1, 0, -1):
+        newx = snake[segment - 1].xcor()
+        newy = snake[segment - 1].ycor()
+        snake[segment].goto(newx, newy)
+
+    head.left(90)
+    head.forward(20)
+
+
 
 screen.exitonclick()
