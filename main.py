f8f751a97af772dcfacd276b8fbc114fc0261827
atrox3d <atrox3d@gmail.com>
2021-02-12 09:25:34 +0100

    add keyboard handlers: my solution

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 gameon = True
 while gameon:
     screen.update()
--- snake.py
+++ snake.py
@@ -8,12 +8,17 @@ STARTING_POSITIONS = [
 
 MOVE_DISTANCE = 20
 
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 
 class Snake:
 
     def __init__(self):
-        self.snake = []
-        self.head = None
+        self.snake: [Turtle] = []
+        self.head: Turtle = None
         self.create_snake()
 
     def create_snake(self):
@@ -35,6 +40,18 @@ class Snake:
             newy = self.snake[segment - 1].ycor()
             self.snake[segment].goto(newx, newy)
 
-        self.head.left(90)
         self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        self.head.setheading(UP)
+
+    def down(self):
+        self.head.setheading(DOWN)
+
+    def left(self):
+        self.head.setheading(LEFT)
+
+    def right(self):
+        self.head.setheading(RIGHT)
+
 
