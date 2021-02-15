4289839fa6fda78a638ce98a272f5771477d9466
atrox3d <atrox3d@gmail.com>
2021-02-15 08:16:28 +0100

    add examples

--- day-24-files/main.py
+++ day-24-files/main.py
@@ -1,13 +1,26 @@
 #
-#   normal
+#   read from file normal
 #
 my_file = open("my_file.txt")
 text = my_file.read()
 print(text)
 my_file.close()
 #
-#   using with
+#   read from file using with
 #
 with open("my_file.txt") as my_file:
     text = my_file.read()
     print(text)
+    # file gets closed automatically
+#
+#   write to file, overwrite
+#
+# with open("my_file.txt") as file:
+#     file.write("overwrite contents")    # io.UnsupportedOperation: not writable
+with open("my_file.txt", mode="w") as file:
+    file.write("overwrite contents")
+#
+#   write to file, append
+#
+with open("my_file.txt", mode="a") as file:
+    file.write("append contents")
