309b5765c067b611d655d310175d5ddadb33dfc7
atrox3d <atrox3d@gmail.com>
2021-02-15 08:28:34 +0100

    add persistent highscore

--- scoreboard.py
+++ scoreboard.py
@@ -9,12 +9,22 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.datafile = "data.txt"
         self.highscore = 0
+        self.load_highscore()
         self.hideturtle()
         self.color("white")
         self.penup()
         self.goto(0, 260)
         self.update_score()
+
+    def load_highscore(self):
+        with open("data.txt") as datafile:
+            self.highscore = int(datafile.read())
+
+    def save_highscore(self):
+        with open("data.txt", mode="w") as datafile:
+            datafile.write(f"{self.highscore}")
 
     def update_score(self):
         self.clear()
@@ -27,6 +37,7 @@ class ScoreBoard(Turtle):
     def reset_score(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            self.save_highscore()
         self.score = 0
         self.update_score()
 
